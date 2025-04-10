614a84956c2947bf0f481c97995be07a92c4d853
Hunor Zoltáni <110656577+Ronuhz@users.noreply.github.com>
2025-04-10 14:30:49 +0300

    Initial Commit

---
+++ TheBrowser/TheBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBrowserApp.swift
+//  TheBrowser
+//
+//  Created by Hunor Zoltáni on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TheBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
